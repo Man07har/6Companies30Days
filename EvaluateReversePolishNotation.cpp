23bf3bff4cea16f96820bcc401f3dea1ac38f764
Man07har <thakurmanoharkumar7@gmail.com>
2023-01-08 12:02:57 +0530

    DAY 1 OF #6Companies30Days #ReviseWithArsh

--- EvaluateReversePolishNotation.cpp
+++ EvaluateReversePolishNotation.cpp
@@ -1,22 +1,41 @@
 class Solution {
-    public int evalRPN(String[] tokens) {
-        int a, b;
-        Stack<Integer> s = new Stack<>();
-        for (String token : tokens) {
-            try {
-                s.add(Integer.parseInt(token));
-            } catch (Throwable e) {
-                a = s.pop();
-                b = s.pop();
-                switch (token) {
-                    case "+": s.add(b + a); break;
-                    case "-": s.add(b - a); break;
-                    case "*": s.add(b * a); break;
-                    case "/": s.add(b / a); break;
-                }
+public:
+    int evalRPN(vector<string>& tokens) {
+        
+        stack<int>myStack;
+        int a1, a2, a3=0;
+        for(int i = 0;i<tokens.size();i++){
+            
+            if( isSymbol(tokens[i])) {  //if symbol, pop from stack and do math
+                 a1 = myStack.top(); myStack.pop();
+                 a2 = myStack.top();myStack.pop();
+                 a3 = applyMath(tokens[i][0],a2, a1);
+                 myStack.push(a3);
+            } else {
+                a3 = stoi(tokens[i]);
+                myStack.push(a3);
             }
         }
-        return s.pop();
+        return a3;
     }
-
+    int applyMath( char symbol, int a1, int a2){
+        int a3 = 0;
+        switch (symbol){
+            case '+': a3 = a1+a2; break;
+            case '*': a3 =  a1*a2; break;
+            case '/': if(a2 != 0) a3 =  a1/a2; else a3 = INT_MAX; break;
+            case '-' : a3 =  a1-a2; break;
+            default: break;
+        }
+        return a3;
+    }
+    bool isSymbol(string ch) {
+        if((ch.length() <= 1) && (ch[0] == '+' || ch[0] == '*' || ch[0] == '/' || ch[0] == '-')){
+           return true;
+        }
+           else {
+           return false;
+        }
+    }
+        
 };
